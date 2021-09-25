68f83a76e2dc3a726dfd04a0096e5bc7eb6e3097
Michael Ryan <michaelryan2895@gmail.com>
2021-09-25 01:15:29 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+from config import *
+from sprites import *
+import sys
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        self.clock = pygame.time.Clock()
